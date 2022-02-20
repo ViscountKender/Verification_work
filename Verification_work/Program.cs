6ac16f0ef79bdd7b54ca7268ed5a56ec719ac8b3
Denis <Siyanov.d@sar.garm.ru>
2022-02-20 17:08:54 +0400

    метод

---
+++ Verification_work/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+void Printgivenarry(string[] givenarry)
+{
+    Write (" [ ");
+    for (int i = 0; i < givenarry.Length; i++)
+    {
+        Write($"\"{givenarry[i]}\"");
+        if(i==givenarry.Length - 1)
+        {
+            Write(" , ");
+        }
+        
+
+
+    }
+    Write(" ] ");
+} 
+
+string[] givenarry = { "1234", ":))","Russia", "-34", "ferst", "Monday", "2 5","0" };
+Printgivenarry(givenarry);
