67a9773a81bd3ac948a6ce9ca8ec46c1b7c771f1
Denis <Siyanov.d@sar.garm.ru>
2022-02-20 18:41:55 +0400

    метод1

--- Verification_work/Program.cs
+++ Verification_work/Program.cs
@@ -2,19 +2,19 @@
 
 void Printgivenarry(string[] givenarry)
 {
-    Write (" [ ");
+    Write ("[");
     for (int i = 0; i < givenarry.Length; i++)
     {
         Write($"\"{givenarry[i]}\"");
-        if(i==givenarry.Length - 1)
+        if(i!=givenarry.Length - 1)
         {
-            Write(" , ");
+            Write(", ");
         }
         
 
 
     }
-    Write(" ] ");
+    Write("] ");
 } 
 
 string[] givenarry = { "1234", ":))","Russia", "-34", "ferst", "Monday", "2 5","0" };
